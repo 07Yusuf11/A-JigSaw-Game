4ec42ae47a57c5190d6be66eb1d6c63216495098
07Yusuf11 <1758042125@qq.com>
2023-02-22 21:53:01 +0800

    Change the block

--- image/rename.sh
+++ image/rename.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for file in *.png; do
-	newfile=$(echo $file | sed -E 's/([0-9]+)-([0-9]+)\.png/Block\1-\2.png/')
+for file in *; do
+	newfile=$(echo $file | sed -E 's/[0-9]+-([0-9]+)/Block\1.png/')
 	mv "$file" "$newfile"
 done
